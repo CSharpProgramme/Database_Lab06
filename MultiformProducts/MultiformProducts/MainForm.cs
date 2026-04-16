a77b65e99c7eb1736e742c18880bed84277cdea5
steve banh <steve.banh02@gmail.com>
2026-04-16 05:57:11 -0400

    lab done

---
+++ MultiformProducts/MultiformProducts/MainForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiformProducts
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void productBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.productBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.productDataSet);
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'productDataSet.Product' table. You can move, or remove it, as needed.
+            this.productTableAdapter.Fill(this.productDataSet.Product);
+
+        }
+
+        private void showDetailsButton_Click(object sender, EventArgs e)
+        {
+            //create an instance of the DetailsForm with current row cumber in GridView
+            DetailsForm details = new DetailsForm(productDataGridView.CurrentCell.RowIndex);
+
+            //Display the form
+            details.ShowDialog();
+
+            //Update the dataset
+            this.productTableAdapter.Fill(this.productDataSet.Product);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //close the form
+            this.Close();
+        }
+    }
+}
